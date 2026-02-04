698a127501ab4e999ca4efd8cca08d10c9523e5f
Юрий <sukonkin.jurii@gmail.com>
2026-02-05 01:16:03 +0600

    Initial commit

---
+++ src/main/java/jurii/cyberguard_server/CyberGuardServerApplication.java
@@ -0,0 +1,13 @@
+package jurii.cyberguard_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberGuardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberGuardServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jurii/cyberguard_server/CyberGuardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jurii.cyberguard_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberGuardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
