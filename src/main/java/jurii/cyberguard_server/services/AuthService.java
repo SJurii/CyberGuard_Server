9da607f26cd13df531c40bbe3f69dfff2f63384c
Iurii <sukonkin.jurii@gmail.com>
2026-02-26 03:26:23 +0600

    Добвлено хэширвоание пароля

--- src/main/java/jurii/cyberguard_server/services/AuthService.java
+++ src/main/java/jurii/cyberguard_server/services/AuthService.java
@@ -1,25 +1,31 @@
 package jurii.cyberguard_server.services;
 
+import jakarta.transaction.Transactional;
 import jurii.cyberguard_server.DTO.Registration;
 import jurii.cyberguard_server.entity.Role;
 import jurii.cyberguard_server.entity.User;
 import jurii.cyberguard_server.repo.UserRepesitory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Transactional
 @Service
 public class AuthService {
     private final UserRepesitory userRepesitory;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepesitory userRepesitory){
+    public AuthService(UserRepesitory userRepesitory, PasswordEncoder passwordEncoder){
         this.userRepesitory = userRepesitory;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registration(Registration request){
         User user = new User();
 
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
         user.setName(request.getName());
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
+        user.setPassword(hashedPassword);
         user.setRole(Role.USER);
 
         return userRepesitory.save(user);
